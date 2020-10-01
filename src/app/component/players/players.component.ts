abc3ee75ecda5674e5a9119a911afe7f0db66041
Freddy Michelena <freddy@michelena.com>
2020-09-30 22:19:25 -0400

    Display player's turn in GameComponent

--- src/app/component/players/players.component.ts
+++ src/app/component/players/players.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GameConfig } from '../../services/game-config';
+import { Player } from '../../classes/player';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  player_1: Player;
+  player_2: Player;
+
+
+  constructor() { 
+    this.player_1 = GameConfig.players.player_1;
+    this.player_2 = GameConfig.players.player_2;
+  }
 
   ngOnInit(): void {
   }
